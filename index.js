1ca01f35a6b4a024c6c3d791eff9e57cd52ba679
AllanEl <allanrnelias@gmail.com>
2022-11-12 04:28:33 -0500

    addEng and addInt functions created

--- index.js
+++ index.js
@@ -4,12 +4,110 @@ const helper = require('./helper')
 employeeArr = [];
 
 const addEng = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'Input',
+        name: 'engname',
+        message: "What is the engineer's name ?"
+      },
+      {
+        type: 'Input',
+        name: 'engid',
+        message: "What is the engineer's ID ?"
+      },
+      {
+        type: 'Input',
+        name: 'engemail',
+        message: "What is the engineer's email ?"
+      },
+      {
+        type: 'Input',
+        name: 'enggit',
+        message: "What is the engineer's GitHub username ?"
+      },
+      
+    ]).then((data) => {
+      employeeArr.push({
+        engName: data.engname,
+        engId: data.engid,
+        engEmail: data.engemail,
+        engGit: data.enggit,
+      })
+      console.log(`${data.engname} is a good engineer`)     
+      console.log(employeeArr);
+      mainMenu();
+    })
 }
 
 const addInt =() => { 
+  inquirer
+  .prompt([
+    {
+      type: 'Input',
+      name: 'intname',
+      message: "What is the intern's name ?"
+    },
+    {
+      type: 'Input',
+      name: 'intid',
+      message: "What is the intern's ID ?"
+    },
+    {
+      type: 'Input',
+      name: 'intemail',
+      message: "What is the intern's email ?"
+    },
+    {
+      type: 'Input',
+      name: 'intgit',
+      message: "What is the intern's GitHub username ?"
+    },
+    
+  ]).then((data) => {
+    employeeArr.push({
+      intName: data.intname,
+      intId: data.intid,
+      intEmail: data.intemail,
+      intGit: data.intgit,
+    })
+    console.log(`${data.intname} is a good engineer`)     
+    console.log(employeeArr);
+    mainMenu();
+  })
 }
 
-const menu = () => {
+const exit =() =>{
+  console.log("goodbye")
+}
+
+const mainMenu = () => {
+  inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: 'What do you want to do ?',
+      name: 'addemployee',
+      choices: ['Add an engineer', 'Add an intern', 'Finish building your team']
+    },
+  ])
+
+  .then((data) => {
+    switch (data.addemployee) {
+    
+      case 'Add an engineer':
+          addEng();
+          break;
+      case 'Add an intern':
+          addInt();
+          break;
+      default:
+          exit();
+    }
+  });
+}
+
+const loginMenu = () => {
 
 inquirer
   .prompt([
@@ -38,26 +136,10 @@ inquirer
       message: "Enter Github username",
       name: 'githubusername'
     },
-    {
-      type: 'list',
-      message: 'What do you want to do ?',
-      name: 'addemployee',
-      choices: ['Add an engineer', 'Add an intern', 'Finish building your team']
-    },
   ])
 
   .then((data) => {
-    switch (data.addemployee) {
-    
-      case 'Add an enginer':
-          addEng()
-          break;
-      case 'Add an intern':
-          addInt()
-          break;
-      default:
-          exit();
-    }
+    mainMenu()
     // console.log({data});
     // const teamMngName = data.teammanager
     // const empID = data.empid
@@ -66,8 +148,8 @@ inquirer
     // const gitHubUser = data.githubusername
     // console.log(teamMngName,empID,email,officeNum,gitHubUser)
     // helper.writeHtmlFile(data)
-  
+    
   });
 }
 
-menu();
+loginMenu();
